9e3cf7441c14530147f6cf1c3e2b344b9af27202
Arthur Van de Wiele <arthur.vandewiele@gmail.com>
2019-07-10 18:15:29 +0200

    feat(test) Add top level collection file to test library.

---
+++ test/collection_full.py
@@ -0,0 +1,8 @@
+# pylint: disable=unused-import,missing-docstring
+
+import unittest
+
+from test.test_main import TestMainOperations
+
+if __name__ == '__main__':
+    unittest.main( verbosity = 99 )
