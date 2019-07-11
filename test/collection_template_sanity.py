51eda03e46be93d388b512cd02c65a9ac1331ad0
Arthur Van de Wiele <arthur.vandewiele@gmail.com>
2019-07-11 13:06:50 +0200

    test(tools) Refactoring template sanity check files.

--- test/collection_template_sanity.py
+++ test/collection_template_sanity.py
@@ -2,8 +2,7 @@
 
 import unittest
 
-from test.test_main import TestMainOperations
-from test.test_tools import TestProjectExplorerLibrary
+from test.test_tools.test_explorer import TestProjectExplorerLibrary
 
 if __name__ == '__main__':
     unittest.main( verbosity = 99 )
