821a544827ec06726de9761ba0df14d2081ac12c
Arthur Van de Wiele <arthur.vandewiele@gmail.com>
2019-07-17 15:29:39 +0200

    refactor(test) Use pytest in place of unittest.

--- test/test_tools/test_pretty_console.py
+++ test/test_tools/test_pretty_console.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import pytest
 from random import choice
 from shutil import get_terminal_size
 from click.testing import CliRunner
@@ -7,8 +7,18 @@ from tools.pretty_console import build_text_in_hline
 from tools.pretty_console import print_subtitle_hline, print_title_hline
 from tools.pretty_console import LEFT, CENTER, RIGHT
 
+POSITION_TEST_DATA = (LEFT, CENTER, RIGHT)
+STRING_TEST_DATA = [
+    'short title',
+    'a somewhat long-ish title',
+    'now this really is a title that noone wants embedded in a string',
+]
 
-class TestPrettyConsole( TestCase ):
+
+class TestPrettyConsole():
+    test_vector = [(string, pos)
+                   for string in STRING_TEST_DATA
+                   for pos in POSITION_TEST_DATA]
 
     def test_command_hline_interface( self ):
         """
@@ -25,44 +35,52 @@ class TestPrettyConsole( TestCase ):
         line_char_bold = '\u2501'
 
         result = runner.invoke( print_title_hline, [string] )
-        self.assertEqual( result.exit_code, 0 )
-        self.assertEqual( len( result.output ), term_len + 1 )  # \n
-        self.assertTrue( line_char_bold in result.output )
+        assert (result.exit_code == 0)
+        assert (len( result.output ) == term_len + 1)  # \n
+        assert (line_char_bold in result.output)
 
         result = runner.invoke( print_subtitle_hline, [string] )
-        self.assertEqual( result.exit_code, 0 )
-        self.assertEqual( len( result.output ), term_len + 1 )  # \n
-        self.assertTrue( line_char_std in result.output )
+        assert (result.exit_code == 0)
+        assert (len( result.output ) == term_len + 1)  # \n
+        assert (line_char_std in result.output)
 
-    def test_various_hline_generations( self ):
+    @pytest.mark.parametrize( 'string, position', test_vector )
+    def test_various_hline_generations( self, string, position ):
         """
         This test verifies the logic behind integrating text in the hline
         using multiple standard configuration vectors.
         """
 
-        position_vector = (LEFT, CENTER, RIGHT)
-        string_vector = [
-            'short title',
-            'a somewhat long-ish title',
-            'now this really is a title that noone wants embedded in a string',
-        ]
+        is_bold = choice( (True, False) )
+        is_using_terminal_width = choice( (True, False) )
 
-        for string in string_vector:
-            for position in position_vector:
-                is_bold = choice( (True, False) )
-                is_using_terminal_width = choice( (True, False) )
+        hline = build_text_in_hline(
+            string,
+            position = position,
+            bold = is_bold,
+            justify = is_using_terminal_width
+        )
 
-                hline = build_text_in_hline(
-                    string,
-                    position = position,
-                    bold = is_bold,
-                    justify = is_using_terminal_width
-                )
+        term_len, _ = get_terminal_size()
+        line_len = 80 if not is_using_terminal_width else term_len
+        line_char = '\u2501' if is_bold else '\u2500'
 
-                term_len, _ = get_terminal_size()
-                line_len = 80 if not is_using_terminal_width else term_len
-                line_char = '\u2501' if is_bold else '\u2500'
+        assert (string in hline)
+        assert (line_char in hline)
+        assert (len( hline ) == line_len)
 
-                self.assertTrue( string in hline )
-                self.assertTrue( line_char in hline )
-                self.assertEqual( len( hline ), line_len )
+    @pytest.mark.xfail( reason = 'Not intended to be used that way.' )
+    def test_hline_generation_with_wrong_arguments( self ):
+        """
+        The HLine methods are not designed to handle wrong arguments gracefully,
+        instead, they will raise errors.
+        """
+
+        hline = build_text_in_hline(
+            'some text',
+            position = 99,  # 0,1,2 are LEFT, CENTER, RIGHT
+            bold = True,
+            justify = False
+        )
+
+        assert isinstance( hline, str )
