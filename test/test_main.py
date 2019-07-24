106d77482c13a069be600e29b038430d2e9e2d74
Arthur Van de Wiele <arthur.vandewiele@gmail.com>
2019-07-24 10:05:19 +0200

    feat(main) Add dummy code and test to increase coverage of the app.main file.

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,35 @@
 """
-Test suite targeting the main calls.
+Test suite targeting the main calls and primary dependencies.
 """
+
 import pytest
 
 from app.main import main
+from app.main import fibonacci
 
 
-class TestMainOperations():
+class TestMainOperations:
 
-    def test_main_returns_properly( self ):
-        ret = main()
-        assert (ret)
+    @pytest.mark.dependency()
+    def test_fibonacci_generator( self ):
+        """
+        The fibonacci generator should return the right fibonacci number
+        for a specific index. Index must me a positive integer.
+        """
+
+        assert fibonacci( -99 ) is None
+        assert fibonacci( 0 ) == 0
+        assert fibonacci( 1 ) == 1
+        assert fibonacci( 2 ) == 1
+        assert fibonacci( 9 ) == 34
+
+    @pytest.mark.dependency(
+        depends = ['TestMainOperations::test_fibonacci_generator'] )
+    def test_main_executes_properly( self ):
+        """
+        During normal execution, the main function returns nothing, but
+        prints out a bunch of stuff. This test ensure that main runs without
+        throwing in exception.
+        """
+
+        assert main() is None
