962cccecdd560748911873dad61b410249e57a6d
Arthur Van de Wiele <arthur.vandewiele@gmail.com>
2019-07-24 10:05:19 +0200

    feat(main) Add dummy code and test to increase coverage of the app.main file.

--- app/main.py
+++ app/main.py
@@ -3,13 +3,33 @@ This is an empty file for the template.
 """
 
 
-def main() -> bool:
+def fibonacci( index: int ) -> int:
+    """
+    Computes the Fibonacci number for a given index.
+    """
+
+    result = None
+
+    if index == 0:
+        result = 0
+    elif index == 1:
+        result = 1
+    elif index > 1:
+        result = fibonacci( index - 1 ) + fibonacci( index - 2 )
+
+    return result
+
+
+def main() -> None:
     """
     This is where the magic happens.
     """
 
-    print( 'Done!' )
-    return True
+    print( 'Computing the first 10 digits of the Fibonacci sequence:' )
+
+    for index in range( 0, 10 ):
+        template = 'fibonacci({index}) = {result}'
+        print( template.format( index = index, result = fibonacci( index ) ) )
 
 
 if __name__ == '__main__':
